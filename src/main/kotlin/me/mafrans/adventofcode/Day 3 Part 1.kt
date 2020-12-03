59e9fdd88f20e396866fe50fefeb074ab520cd58
Mafrans <malte.kluft@gmail.com>
2020-12-03 15:46:00 +0100

    Finish Day 3 Part 1

--- src/main/kotlin/me/mafrans/adventofcode/Day 3 Part 1.kt
+++ src/main/kotlin/me/mafrans/adventofcode/Day 3 Part 1.kt
@@ -6,18 +6,23 @@ fun main() {
     // Start timer once the input has been read, so that it only measures the algorithm itself
     Timer.start()
 
+    // Amount of trees
     var count = 0;
-    var y = 0;
-    var x = 0;
 
-    while (x < input.size && y < input[0].length) {
-        if(input[x][y] == '#') {
+    // X-coordinate, from left to right
+    var x = 0
+
+    // Y-coordinate, from top to bottom
+    var y = 0
+
+    // Loop and add 1 to `y` until it reaches the maximum value
+    while(y < input.size) {
+        // If the character at position (x, y) is a tree, increase the count
+        if(input[y][x] == '#')
             count++;
-        }
 
-        println()
-
-        x += 3;
+        // Add 3 to the x coordinate, looping from the beginning if it reaches the max length
+        x = (x + 3) % input[0].length;
         y += 1;
     }
 
