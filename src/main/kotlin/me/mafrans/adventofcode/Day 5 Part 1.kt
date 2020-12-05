a5be9f2d03dc85231a1f14b3141e2fc7e51987b7
Mafrans <malte.kluft@gmail.com>
2020-12-05 22:29:55 +0100

    Comment day 5 parts 1 and 2

--- src/main/kotlin/me/mafrans/adventofcode/Day 5 Part 1.kt
+++ src/main/kotlin/me/mafrans/adventofcode/Day 5 Part 1.kt
@@ -8,9 +8,15 @@ fun main() {
     // Start timer once the input has been read, so that it only measures the algorithm itself
     Timer.start()
 
+    // The highest seat id
     var highest = 0;
 
     for (line in input)  {
+        /*
+        *   Calculate the row by procedurally reducing the maximum and increasing the minimum
+        *   If 'F', decrease the maximum by half the difference between maximum and minimum (rounding up)
+        *   If 'B', increase the minimum by half the difference between maximum and minimum (rounding up)
+        * */
         var rowMin = 0
         var rowMax = 127
         for (char in line.substring(0, 7)) {
@@ -20,6 +26,11 @@ fun main() {
             }
         }
 
+        /*
+        *   Calculate the seat by procedurally reducing the maximum and increasing the minimum
+        *   If 'L', decrease the maximum by half the difference between maximum and minimum (rounding up)
+        *   If 'R', increase the minimum by half the difference between maximum and minimum (rounding up)
+        * */
         var seatMin = 0
         var seatMax = 7
         for (char in line.substring(7, 10)) {
@@ -29,7 +40,10 @@ fun main() {
             }
         }
 
+        // Calculate the seat id hash
         val id = rowMax * 8 + seatMax
+
+        // If the id hash is higher than the current highest, set the current highest to the id hash
         if (id > highest)
             highest = id
     }
--- src/main/kotlin/me/mafrans/adventofcode/Day 5 Part 2.kt
+++ src/main/kotlin/me/mafrans/adventofcode/Day 5 Part 2.kt
@@ -8,15 +8,23 @@ fun main() {
     // Start timer once the input has been read, so that it only measures the algorithm itself
     Timer.start()
 
+    // A hashset containing all ids included in the input
     val ids = HashSet<Int>()
 
+    // Generate all ids available in the input, and add them to the hashset
     for (line in input)  {
         val rowData = line.substring(0, 7)
         val seatData = line.substring(7, 10)
 
+        // If the row is all the way in the front or all the way to the back, ignore it
         if (rowData == "FFFFFFF" || rowData == "BBBBBBB")
             continue
 
+        /*
+        *   Calculate the row by procedurally reducing the maximum and increasing the minimum
+        *   If 'F', decrease the maximum by half the difference between maximum and minimum (rounding up)
+        *   If 'B', increase the minimum by half the difference between maximum and minimum (rounding up)
+        * */
         var rowMin = 0
         var rowMax = 127
         for (char in rowData) {
@@ -26,6 +34,11 @@ fun main() {
             }
         }
 
+        /*
+        *   Calculate the seat by procedurally reducing the maximum and increasing the minimum
+        *   If 'L', decrease the maximum by half the difference between maximum and minimum (rounding up)
+        *   If 'R', increase the minimum by half the difference between maximum and minimum (rounding up)
+        * */
         var seatMin = 0
         var seatMax = 7
         for (char in seatData) {
@@ -35,12 +48,18 @@ fun main() {
             }
         }
 
+        // Add the id hash to the ´ids´ list
         ids += rowMax * 8 + seatMax
     }
 
+    // Loop over every possible row, ignoring the frontmost row and backmost row
     outer@for (row in 1..126) {
+        // Loop over every possible seat, ignoring the leftmost seat and the rightmost seat
         for (seat in 1..7) {
+            // Calculate the seat id
             val id = row * 8 + seat
+
+            // If the id is not in our input but the adjacent ids are, we've found the right seat
             if (!ids.contains(id) && ids.contains(id + 1) && ids.contains(id - 1)) {
                 println(id)
                 break@outer
